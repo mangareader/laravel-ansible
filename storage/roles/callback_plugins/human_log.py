ceb23de6766640491cb6b77b13194b6b460d9535
Hieunt <ng.tr.hieu@gmail.com>
2016-02-03 14:43:11 +0700

    init

---
+++ storage/roles/callback_plugins/human_log.py
@@ -0,0 +1,40 @@
+FIELDS = ['stdout']
+url="http://test.com/jobs/run/"
+import requests
+import json
+
+pid=-1
+class CallbackModule(object):
+    def playbook_on_play_start(self, pattern):
+        global pid
+        pid = self.play.vars.get("pid")
+        requests.post(url+str(pid),
+            data = {"pname":pattern,"name":"play_start"})
+    def playbook_on_task_start(self, name, is_conditional):
+        requests.post(url+str(pid),
+            data = {"tname":name,"name":"task_start"})
+    def runner_on_unreachable(self, host, res):
+		requests.post(url+str(pid),
+            data = {"host":host,"name":"unreachable","res":res})
+    def runner_on_failed(self, host, res):
+		requests.post(url+str(pid),
+            data = {"host":host,"name":"failed","res":res})
+    def runner_on_skipped(self, host, item=None):
+		requests.post(url+str(pid),
+            data = {"host":host,"name":"skipped"})
+    def runner_on_ok(self, host, res):
+        requests.post(url+str(pid),
+            data = {"host":host,"name":"ok","changed":res.get("changed")})
+    def playbook_on_stats(self, stats):
+        list=[]
+        for key in stats.processed:
+            tmp={
+                    "host":key, "ok":stats.ok.get(key),
+                    "failures":stats.failures.get(key),
+                    "dark":stats.dark.get(key),
+                    "skipped":stats.skipped.get(key),
+                    "changed":stats.changed.get(key)
+                }
+            list.append(tmp)
+        requests.post(url+str(pid),
+            data = {"data":json.dumps(list),"name":"on_stats"})
